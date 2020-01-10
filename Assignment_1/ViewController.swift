f068f0e2610292bcee8489b2bd5eda6f41f193c9
kumarkay.ca@gmail.com <Karan>
2020-01-10 18:38:42 -0500

    changes

--- Assignment_1/ViewController.swift
+++ Assignment_1/ViewController.swift
@@ -18,19 +18,114 @@ class ViewController: UIViewController {
         
         self.imgView.frame.origin.y = self.space.bounds.minY
         self.imgView.frame.origin.x = self.space.bounds.minX
+   
+        UIView.animate(withDuration: 2, animations: {
+                               
+                                if self.imgView.frame.origin.y == self.space.bounds.minY && self.imgView.frame.origin.x == self.space.bounds.minX
+                                {
+                    //            print(self.space.bounds.minX)
+                    //            print(self.space.bounds.minY)
+                                self.imgView.frame.origin.y += self.space.bounds.height - self.imgView.bounds.height
+        //                            print(self.space.bounds.height - self.imgView.bounds.height)
+        //                            print(self.imgView.frame.origin.y)
+        //                            print("\n")
+        //                            print( self.imgView.frame.origin.x)
+                                    
+                                }
+                              
+                            
+                            
+                            },completion: nil)
         
-        move()
-        
-       
-      
+          let swipeRight=UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+              swipeRight.direction=UISwipeGestureRecognizer.Direction.right
+              view.addGestureRecognizer(swipeRight)
+              
+              let swipeLeft=UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+              swipeLeft.direction=UISwipeGestureRecognizer.Direction.left
+              view.addGestureRecognizer(swipeLeft)
         
         
       
         
         
     }
+     @objc func swiped(gesture:UISwipeGestureRecognizer)
+       {
+          self.imgView.frame.origin.y = self.space.bounds.minY
+          self.imgView.frame.origin.x = self.space.bounds.minX
+          
+          let swipeGesture=gesture as? UISwipeGestureRecognizer
+           switch swipeGesture?.direction {
+            
+               case UISwipeGestureRecognizer.Direction.right:
+                   moveclockwise()
+               break
+                   case UISwipeGestureRecognizer.Direction.left:
+                  moveCounterClockwise()
+               break
+               default:
+                   break
+               }
+           
+       }
     
-    func move() -> Void {
+    
+  func moveCounterClockwise()
+        {
+            self.imgView.frame.origin.y = self.space.bounds.minY
+                    self.imgView.frame.origin.x = self.space.bounds.minX
+                    
+                       
+            UIView.animate(withDuration: 1, animations: {
+                       
+                        if self.imgView.frame.origin.y == self.space.bounds.minY && self.imgView.frame.origin.x == self.space.bounds.minX
+                        {
+                            print(self.imgView.isAnimating)
+            //            print(self.space.bounds.minX)
+            //            print(self.space.bounds.minY)
+                             self.imgView.frame.origin.x += self.space.bounds.width - self.imgView.bounds.width
+                      //  self.imageView.frame.origin.y += self.space.bounds.height - self.imageView.bounds.height
+                            
+                            
+                        }
+                      
+                    
+                    
+                    },completion:{ (true) in
+                        UIView.animate(withDuration: 1, animations: {
+                            if self.imgView.frame.origin.y == self.space.bounds.minY && self.imgView.frame.origin.x == self.space.bounds.width - self.imgView.bounds.width
+    //
+                                      {
+                                         self.imgView.frame.origin.y += self.space.bounds.height - self.imgView.bounds.height
+                                        
+                                       
+                                                }
+                        },completion: {
+                            (true) in
+                            UIView.animate(withDuration: 1, animations: {
+                                if self.imgView.frame.origin.y == self.space.bounds.height - self.imgView.bounds.height && self.imgView.frame.origin.x == self.space.bounds.width - self.imgView.bounds.width
+                                          {
+                                            self.imgView.frame.origin.x -= self.space.bounds.width - self.imgView.bounds.width
+                                                    }
+                            },completion:{
+                                (true) in
+                                UIView.animate(withDuration: 1, animations: {
+                                   if  self.imgView.frame.origin.y == self.space.bounds.height - self.imgView.bounds.height && self.imgView.frame.origin.x == self.space.bounds.minX
+                                   
+                                              {
+                                                    
+                                                self.imgView.frame.origin.y -= self.space.bounds.height - self.imgView.bounds.height
+                                               
+                                                        }
+                                },completion:nil)
+                            })
+                            
+                        })
+                    })
+        }
+    
+    func moveclockwise() -> Void {
        
                      UIView.animate(withDuration: 2, animations: {
                                
@@ -70,9 +165,8 @@ class ViewController: UIViewController {
 //                                                        print(self.space.bounds.minY)
 //                                                        print(self.space.bounds.minX)
                                                                 }
-                                        },completion: {(true) in
-                                            self.move()
-                                        })
+                                        },completion: nil)
+                                        
                                     })
                                     
                                 })
